b4f579e602337aa0b6726debe55655680cd2264e
Nasr Siddiqui <nasr.siddiqui1@gmail.com>
2023-09-25 10:17:34 -0700

    Replacing all of my 'under the hood' TF code with an external module that does all the work for me

---
+++ backup/terraform.tfvars
@@ -0,0 +1,4 @@
+repo_name = "thebestrepo"
+overwrite_on_create = false
+existing_repo_name = "terraform_datasource_repo"
+create_file_in_repo = true 
--- main.tf
+++ main.tf
@@ -10,22 +10,11 @@ terraform {
 # Configure the GitHub Provider
 provider "github" {}
 
-resource "github_repository" "repo" {
-  name        = var.repo_name
-  description = "My awesome codebase"
-  visibility = "public"
-}
+module "repository" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
 
-resource "github_repository_file" "file" {
-  #condition ? true_val : false_val
-  count = var.create_file_in_repo ? 1 : 0
-
-
-  repository          = github_repository.repo.name
-  file                = "test"
-  content             = "Test contents"
-  commit_message      = "Managed by Terraform"
-  commit_author       = "Terraform User"
-  commit_email        = "terraform@example.com"
-  overwrite_on_create = var.overwrite_on_create
+  name               = "terraform-github-repository"
+  license_template   = "apache-2.0"
+  gitignore_template = "Terraform"
 }
